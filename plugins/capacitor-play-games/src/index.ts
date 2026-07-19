8171902be3085f385526e0c7748eaf1ddfde1ddf
flaggameapp <flaggameapp@gmail.com>
2026-07-19 13:42:40 -0300

    Delete plugins/capacitor-play-games directory

--- plugins/capacitor-play-games/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from "@capacitor/core";
-import type { FlagGamePlayGamesPlugin } from "./definitions";
-
-const FlagGamePlayGames = registerPlugin<FlagGamePlayGamesPlugin>(
-  "FlagGamePlayGames"
-);
-
-export * from "./definitions";
-export { FlagGamePlayGames };
